8bef0989bfd852ca8ae762c0219ad112fccce62b
valeria campilongo <cvaleriac@hotmail.it>
2019-03-19 21:08:27 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_by(uid: auth['uid']) do |u|
       u.name = auth['info']['name']
-    
+
     end
 
     session[:user_id] = @user.id
